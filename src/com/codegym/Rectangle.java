8937f0f320bbc50fe9c274d1408e1dd5c547a71b
thaophamhetxat <thaophamhetxat@gmail.com>
2021-06-08 17:10:18 +0700

    lop hinh chữ nhật

---
+++ src/com/codegym/Rectangle.java
@@ -0,0 +1,34 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    double width,height;
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea(){
+        return this.width*this.height;
+    }
+    public double getPerimeter(){
+        return (this.width + this.height)*2;
+    }
+    public String display(){
+        return "Rectangle{"+"width="+width+",height="+height+"}";
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the width:");
+        double width = scanner.nextDouble();
+        System.out.println("enter the height:");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width,height);
+        System.out.println("your rectangle \n"+rectangle.display());
+        System.out.println("perimeter of the rectangle: "+rectangle.getPerimeter());
+        System.out.println("area of the rectangle: "+rectangle.getArea());
+    }
+}
